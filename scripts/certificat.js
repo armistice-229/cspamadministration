3b16523de3a2243b6abde075e25cc682930aa36a
armistice-229 <armistice095@gmail.com>
2026-03-20 17:39:23 +0100

    Change FedaPay public key to sandbox
    Updated FedaPay public key to sandbox for testing.

--- scripts/certificat.js
+++ scripts/certificat.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
  
   // 👉 On initialise le widget FedaPay une seule fois
   FedaPay.init("#generate-btn", {
-    public_key: "pk_live_2dkPm2oKI6Jl9fDN2tnfmCN7", // Mets la clé live en prod
+    public_key: "pk_sandbox_MzRYR_dNY8mLjA8BKJHxaV0I", // Mets la clé live en prod
     transaction: {
       description: "Certificat de scolarité",
       amount: 295,
